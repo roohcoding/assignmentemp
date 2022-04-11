934d7fdf20aa9edec3c4624c46dc389634e5d8ff
Rupali Natewad <rupalirnatewad@gmail.com>
2022-04-12 00:43:55 +0530

    uploading Uc1_Attendance file

---
+++ EmpComputation/EmpComputation/Uc1-attendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpComputation
+{
+    internal class Uc1_attendance
+    {
+
+        public static void Attendance()
+        {
+            int Is_Full_Time = 1;
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if (num == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
